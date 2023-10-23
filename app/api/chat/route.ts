5d207c391bfcadd8a5a18e5b70be15ea988504fc
Nathan Laundry <nathan.laundry@gmail.com>
2023-10-23 13:42:37 -0400

    initial political party form submission and prompt demo

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,60 +8,72 @@ import { nanoid } from '@/lib/utils'
 export const runtime = 'edge'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration)
 
 export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+    const json = await req.json()
+    let { seedMessages, messages, previewToken } = json
+    console.log(json)
+    console.log("yo what the fuck??")
+    const userId = (await auth())?.user.id
 
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
-    })
-  }
-
-  if (previewToken) {
-    configuration.apiKey = previewToken
-  }
-
-  const res = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
+    if (!userId) {
+        return new Response('Unauthorized', {
+            status: 401
+        })
     }
-  })
 
-  return new StreamingTextResponse(stream)
+    if (previewToken) {
+        configuration.apiKey = previewToken
+    }
+
+    // const seedPrompt = {
+    //     role: 'system',
+    //     content: 'You are a political conservative who is open to having a fair and friendly discussion with political partisans who do not share your views. You share your moral values and beliefs openly and honestly and invite others to do the same. When there are disagreements, you present your view and invite others to do the same.'
+    // };
+
+    messages = [...seedMessages, ...messages];
+
+    const res = await openai.createChatCompletion({
+        model: 'gpt-4',
+        messages,
+        temperature: 0.7,
+        stream: true
+    })
+
+    // console.log(messages)
+
+
+    const stream = OpenAIStream(res, {
+        async onCompletion(completion) {
+            const title = json.messages[0].content.substring(0, 100)
+            const id = json.id ?? nanoid()
+            const createdAt = Date.now()
+            const path = `/chat/${id}`
+            const payload = {
+                id,
+                title,
+                userId,
+                createdAt,
+                path,
+                messages: [
+                    ...messages,
+                    {
+                        content: completion,
+                        role: 'assistant'
+                    }
+                ]
+            }
+            await kv.hmset(`chat:${id}`, payload)
+            await kv.zadd(`user:chat:${userId}`, {
+                score: createdAt,
+                member: `chat:${id}`
+            })
+        }
+    })
+
+    return new StreamingTextResponse(stream)
 }
--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,56 +1,133 @@
-import { UseChatHelpers } from 'ai/react'
+import React, { useState } from 'react';
+import { UseChatHelpers } from 'ai/react';
+import { Message } from 'ai'
 
-import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/external-link'
-import { IconArrowRight } from '@/components/ui/icons'
+export interface FormState {
+    userAlignment: string;
+    partnerAlignment: string;
+    discussionTopics: string[];
+}
 
-const exampleMessages = [
-  {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
-  },
-  {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
-  },
-  {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
-]
 
-export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
-  return (
-    <div className="mx-auto max-w-2xl px-4">
-      <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-        </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
-        <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
-        </p>
-        <div className="mt-4 flex flex-col items-start space-y-2">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              onClick={() => setInput(message.message)}
-            >
-              <IconArrowRight className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
+const politicalAlignments = [
+    'Conservative',
+    'Liberal',
+    'Libertarian',
+    'Socialist',
+];
+
+const discussionTopics = [
+    'Healthcare',
+    'Immigration',
+    'Taxes',
+    'Climate Change',
+];
+
+export function EmptyScreen({ onSubmitForm }) {
+    const [formState, setFormState] = useState<FormState>({
+        userAlignment: '',
+        partnerAlignment: '',
+        discussionTopics: []
+    });
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = event.target;
+        setFormState(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleMultiSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const options = event.target.options;
+        const values: string[] = [];
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].selected) {
+                values.push(options[i].value);
+            }
+        }
+        setFormState(prevState => ({
+            ...prevState,
+            discussionTopics: values
+        }));
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const seedPrompt = {
+            role: 'system',
+            content: `You are a political ${formState.partnerAlignment} who is open to having a fair and friendly discussion with political partisans who do not share your views such as ${formState.userAlignment}. You are interested in the following topics ${formState.discussionTopics.join(', ')}. You share your moral values and beliefs openly and honestly and invite others to do the same. When there are disagreements, you present your view and invite others to do the same.`
+        };
+        const firstMessage = {
+            role: 'assistant',
+            content: `Hi, I'm a ${formState.partnerAlignment}. I hear your political views are ${formState.userAlignment}. I would love the opportunity to have an open and fair discussion about the topics you listed: ${formState.discussionTopics} and anything else you'd like to get a ${formState.partnerAlignment}
+s views on.`
+        };
+        let messages = [seedPrompt, firstMessage]
+        onSubmitForm(messages)
+    };
+
+    return (
+        <div className="mx-auto max-w-2xl px-4">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label htmlFor="userAlignment" className="block mb-2">Your Political Alignment</label>
+                    <select
+                        name="userAlignment"
+                        id="userAlignment"
+                        value={formState.userAlignment}
+                        onChange={handleInputChange}
+                        className="border rounded p-2 w-full"
+                    >
+                        {politicalAlignments.map(align => (
+                            <option key={align} value={align}>{align}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="partnerAlignment" className="block mb-2">Conversational Partner's Alignment</label>
+                    <select
+                        name="partnerAlignment"
+                        id="partnerAlignment"
+                        value={formState.partnerAlignment}
+                        onChange={handleInputChange}
+                        className="border rounded p-2 w-full"
+                    >
+                        {politicalAlignments.map(align => (
+                            <option key={align} value={align}>{align}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="discussionTopics" className="block mb-2">Preferred Discussion Topics</label>
+                    <select
+                        name="discussionTopics"
+                        id="discussionTopics"
+                        multiple
+                        value={formState.discussionTopics}
+                        onChange={handleMultiSelectChange}
+                        className="border rounded p-2 w-full"
+                    >
+                        {discussionTopics.map(topic => (
+                            <option key={topic} value={topic}>{topic}</option>
+                        ))}
+                    </select>
+                    {formState.discussionTopics.includes('Other') && (
+                        <input
+                            type="text"
+                            placeholder="Specify other topics"
+                            name="discussionTopicsOther"
+                            className="border rounded p-2 w-full mt-2"
+                            onChange={handleInputChange}
+                        />
+                    )}
+                </div>
+
+                <button type="submit" className="bg-blue-500 text-white rounded p-2">Submit</button>
+            </form>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
